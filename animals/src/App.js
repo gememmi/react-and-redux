eab8960e2607dbda1c3844d0665dee76449cece7
Emily <emelizadaniels@gmail.com>
2023-09-08 17:50:34 -0400

    idk

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import { useState } from 'react';
+
+function getRandomAnimal(){
+    const animals  = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+    return animals[Math.floor(Math.random() * animals.length)]
+}
+// to clarify, Math.random() get a random float from 0-1, that why we can multiply by the length, round down with Math.floor() and still get a valid index
+
 
 function App(){
-    
-    const handleClick = () => {
-        console.log('button was clicked!');
-    }
+    const [animals, setAnimals] = useState([]);
+
+    function handleClick(){
+        setAnimals([...animals, getRandomAnimal()])
+    };
     
     return(
     <div>
         <button onClick={handleClick}>Add Animal</button>
+        <div>{animals}</div>
+        
     </div>
     )
 }
