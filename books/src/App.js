4db38dc4a1db296a9bf3779597b1cd1045bb94be
Emily <emelizadaniels@gmail.com>
2023-09-25 16:15:15 -0400

    init books app

---
+++ books/src/App.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+function App() {
+    const [books, setBooks] = useState([]);
+    return (
+    <div>
+        <div>App</div>
+        
+        
+    </div>
+    );
+}
+
+export default App;
