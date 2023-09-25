6a96311ed8e9ca7323e7ae2800b96f90e633ef0c
Emily <emelizadaniels@gmail.com>
2023-09-25 19:07:39 -0400

    spread operator to add more data to the dom

--- books/src/App.js
+++ books/src/App.js
@@ -1,10 +1,18 @@
 import { useState } from 'react';
+import BookCreate from './components/BookCreate';
 
 function App() {
     const [books, setBooks] = useState([]);
+
+    const createBook = (title) => {
+
+    }
+        
     return (
     <div>
-        <div>App</div>
+        <div>
+            <BookCreate  onCreate = {createBook} />
+        </div>
         
         
     </div>
@@ -12,3 +20,23 @@ function App() {
 }
 
 export default App;
+
+// Adding and editing information on the DOM 
+
+// To update state with a new object, add a new object to the empty array in our state:
+//CREATE A NEW ARRAY
+//COPY ALL THE ELEMENTS FROM THE OLD ARRAY
+//ADD NEW ELEMENTS TO THE END
+// This alerts React that the new array differes from the old array and that it should rerender the screen!
+//The spread operator allows us to creata copy of the old array, and then add the new object
+
+// TO ADD AN ELEMENT AT A SPECIFIC INDEX:
+//INSERT 'purple' at index 1
+//[
+
+//     ...colors.slice(0,1),
+// 'purple',
+// ...colors.slice(1)
+// ];
+
+// HOW TO REMOVE AN ELEMENT FROM USING .FILTER
--- books/src/components/BookCreate.js
+++ books/src/components/BookCreate.js
@@ -1,5 +1,35 @@
-function BookCreate(){
-    return <div>BookCreate</div>
+import { useState } from 'react';
+
+function BookCreate({ onCreate }){
+    const [title, setTitle] = useState('');
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onCreate(title)
+        setTitle('');
+    }
+
+    return (
+    <div className= "book-create">
+        <h3>Add a Book</h3>
+        <form onSubmit={handleSubmit}>
+            <label>Title</label>
+            <input className="input" value={title} onChange= {handleChange} />
+            <button className="button">Create!</button>
+        </form>
+    </div>
+    )
 }
 
 export default BookCreate;
+
+// Text Inputs are alwasy controlled Components!!
+// Remembe, we do an onChange to the input to handle the change of state
+// And then we do an onSubmit to the form to pas the data back to our App to actually create a NEW object to render on the screen.
+
+ //CLEAR OUT THE TEXT
+ //set the new state to an empty string
