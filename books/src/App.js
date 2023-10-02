3fe88f5f72e5dd3427ae0748bfd64de9d4722d3e
Emily <emelizadaniels@gmail.com>
2023-10-02 19:49:53 -0400

    added delete function, rendered books as we add them

--- books/src/App.js
+++ books/src/App.js
@@ -1,16 +1,29 @@
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 
 function App() {
     const [books, setBooks] = useState([]);
 
     const createBook = (title) => {
+        const updatedBooks = [...books, {
+            id: Math.round(Math.random() * 9999)
+            , 
+            title}];
+        setBooks(updatedBooks);
+    };
 
-    }
+    const deleteBookById = (id) => {
+       const updatedBooks = books.filter((book) => {
+            return book.id !== id;
+        })
+        setBooks(updatedBooks);
+    };
         
     return (
     <div>
-        <div>
+        <div className= "app">
+            <BookList books={books} onDelete={deleteBookById}/>
             <BookCreate  onCreate = {createBook} />
         </div>
         
