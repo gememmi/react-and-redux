6b6d6afcda9e98f89ad6983a1c3d3703efb9f95f
Emily <emelizadaniels@gmail.com>
2023-10-02 20:13:18 -0400

    added onChange and onSubmit for the edit button

--- books/src/components/BookShow.js
+++ books/src/components/BookShow.js
@@ -1,13 +1,27 @@
+import { useState } from 'react';
+import BookEdit from './BookEdit';
+
 function BookShow({ book, onDelete }){
+    const [showEdit, setShowEdit] = useState(false);
+
+    const handleDeleteClick = () => {
+        onDelete(book.id);
+    }
+    const handleEditClick =  () => {
+        setShowEdit(!showEdit);
+        
+    }
     
-const handleClick = () => {
-    onDelete(book.id);
-}
-    
+    let content = <h3>{book.title}</h3>;
+    if (showEdit) {
+        content = <BookEdit book={book} />;
+    }
+
     return <div className="book-show">
-        {book.title}
+        <div>{content}</div>
         <div className="actions">
-            <button className = "delete" onClick={handleClick}>
+            <button className="edit" onClick ={handleEditClick} >Edit</button>
+            <button className = "delete" onClick={handleDeleteClick}>
                 Delete
             </button>
         </div>
