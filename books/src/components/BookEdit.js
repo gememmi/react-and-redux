b4dd3d6991a5dda1dadd12dee88e4c8f9a3b9a1e
Emily <emelizadaniels@gmail.com>
2023-10-13 10:55:25 -0400

    completing edit function adding picsum images

--- books/src/components/BookEdit.js
+++ books/src/components/BookEdit.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-function BookEdit({ book }){
+function BookEdit({ book, onSubmit }){
 
     const [title, setTitle] = useState(book.title);
 
@@ -10,7 +10,7 @@ function BookEdit({ book }){
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log('New Title', title);
+        onSubmit(book.id, title)
     }
 
     return( 
--- books/src/components/BookShow.js
+++ books/src/components/BookShow.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import BookEdit from './BookEdit';
 
-function BookShow({ book, onDelete }){
+function BookShow({ onEdit, onDelete, book }){
     const [showEdit, setShowEdit] = useState(false);
 
     const handleDeleteClick = () => {
@@ -11,16 +11,25 @@ function BookShow({ book, onDelete }){
         setShowEdit(!showEdit);
         
     }
+
+    const handleSubmit = (id, newTitle) => {
+        setShowEdit(false);
+        onEdit(id, newTitle);
+    }
     
     let content = <h3>{book.title}</h3>;
     if (showEdit) {
-        content = <BookEdit book={book} />;
+        content = <BookEdit book={book} onSubmit = {handleSubmit} />;
     }
 
     return <div className="book-show">
+        <img
+        alt="books"
+        src={`https://picsum.photos/seed/${book.id}/300/200`}
+        />
         <div>{content}</div>
         <div className="actions">
-            <button className="edit" onClick ={handleEditClick} >Edit</button>
+            <button className="edit" onClick ={handleEditClick} onSubmit= {handleSubmit}>Edit</button>
             <button className = "delete" onClick={handleDeleteClick}>
                 Delete
             </button>
