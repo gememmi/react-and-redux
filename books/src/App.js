8100a9167b6b0a9d95aa25049c024be24ce4ef60
Emily <emelizadaniels@gmail.com>
2023-10-17 11:41:30 -0400

    added get, put, post, delete request with useEffect

--- books/src/App.js
+++ books/src/App.js
@@ -1,29 +1,53 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
 function App() {
     const [books, setBooks] = useState([]);
 
-    const createBook = (title) => {
-        const updatedBooks = [...books, {
-            id: Math.round(Math.random() * 9999)
-            , 
-            title}];
-        setBooks(updatedBooks);
-    };
+    //GET all the books in our db.json
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
 
-    const deleteBookById = (id) => {
+        setBooks(response.data);
+    };
+    //call GET request only when the page first renders
+    useEffect (() => {
+        fetchBooks();
+    }, []);
+
+    //POST request to db.json
+    const createBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books',{
+            title
+        });
+        const updatedBooks = [
+            ...books,
+            response.data
+        ];
+        setBooks(updatedBooks);
+        
+    };
+    //DELETE request to db.json
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
        const updatedBooks = books.filter((book) => {
             return book.id !== id;
         })
         setBooks(updatedBooks);
     };
 
-    const editBookById = (id, newTitle) => {
+    //PUT request to db.json
+    const editBookById = async (id, newTitle) => {
+       const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle
+        });
+        console.log(response)
+
         const updatedBooks = books.map((book) => {
             if (book.id === id ) {
-                return {...book, title: newTitle};
+                return {...book, ...response.data};
             }
             return book;
         })
@@ -64,3 +88,9 @@ export default App;
 // ];
 
 // HOW TO REMOVE AN ELEMENT FROM USING .FILTER
+
+// USEEFFECT:
+// USED TO RUN CODE AT VERY  SPECIFIC TIMES, LIKE WHEN THE COMPONENT IS INTIALLY RENDERED. AND SOMETIMES WHEN IT IS RERENDERED. 
+//The first argument is a function that contains code we want to run and the second is an array or nothing -- this controls whether the funciton is executed on rerenders. So it should either be an empty array or have some numbers in it
+//When does the arrow function get called?-- Immediately after the first render, any other times depends on the second argument (empty array or not)
+//What is the arrow functions return value?
