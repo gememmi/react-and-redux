185be0be46be5b3c42f4d87a299160b840a56372
Emily <emelizadaniels@gmail.com>
2023-11-19 20:18:11 -0500

    added classnames

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,24 +1,28 @@
 import Button from './Button';
+import { GoBell } from "react-icons/go";
 
 function App() {
     return <div>
         <div>
-            <Button>Hello</Button>
+            <Button primary outline >
+                <GoBell />
+                Hello
+                </Button>
         </div>
         <div>
-            <Button >What the heck is up</Button>
+            <Button secondary outline >What the heck is up</Button>
         </div>
         <div>
-            <Button>Barf</Button>
+            <Button warning rounded >Barf</Button>
         </div>
         <div>
-            <Button>I LVOE YOU</Button>
+            <Button danger >I LVOE YOU</Button>
         </div>
         <div>
-            <Button>UMMMM ok</Button>
+            <Button success >UMMMM ok</Button>
         </div>
         <div>
-            <Button>nasty</Button>
+            <Button rounded>nasty</Button>
         </div>
     </div>
 }
